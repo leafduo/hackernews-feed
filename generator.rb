91d597789308e8958aba9cf849578cf793f14a87
Zuyang Kou <leafduo@gmail.com>
2012-08-29 02:37:40 +0800

    A good start.

---
+++ generator.rb
@@ -0,0 +1,22 @@
+require 'atom'
+require 'feedzirra'
+require 'ruby-hackernews'
+
+feed = Atom::Feed.new do |f|
+    f.title = "Hacker News"
+    f.links << Atom::Link.new(:href => "http://news.ycombinator.com/")
+    f.updated = Time.now
+    f.authors << Atom::Person.new(:name => 'Customized by leafduo.com')
+    f.id = "leafduo.com"
+    RubyHackernews::Entry.all.each do |post|
+        f.entries << Atom::Entry.new do |e|
+            e.title = post.link.title
+            e.links << Atom::Link.new(:href => post.link.href)
+            e.id = post.link.href
+            #e.updated = post.time # FIXME: Due to a bug in ruby-hackernews, this can crash.
+            #e.summary = "Some text."
+        end
+    end
+end
+
+#puts feed.to_xml
