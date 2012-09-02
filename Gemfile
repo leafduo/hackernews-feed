ad1b9eaada92d3f3ed52954022be1eecebb8fba8
Zuyang Kou <leafduo@gmail.com>
2012-09-03 01:43:24 +0800

    Add readability support.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem "ratom", "~> 0.7.2"
 gem "feedzirra", "~> 0.1.3"
 gem "ruby-hackernews", "~> 1.3.0"
 gem 'whenever', :require => false
+gem "ruby-readability", :require => 'readability'
--- generator.rb
+++ generator.rb
@@ -1,6 +1,8 @@
 require 'atom'
 require 'feedzirra'
 require 'ruby-hackernews'
+require 'readability'
+require 'open-uri'
 
 feed = Atom::Feed.new do |f|
     f.title = "Hacker News"
@@ -11,9 +13,16 @@ feed = Atom::Feed.new do |f|
     RubyHackernews::Entry.all.each do |post|
         f.entries << Atom::Entry.new do |e|
             e.title = post.link.title + ' (' + post.voting.score.to_s + ')'
+            puts e.title
             e.links << Atom::Link.new(:href => post.link.href)
             e.id = post.link.href
             #e.updated = post.time # FIXME: Due to a bug in ruby-hackernews, this can crash.
+            begin
+                original_content = open(post.link.href).read
+                e.content = Readability::Document.new(original_content).content
+            rescue
+                e.content = ''
+            end
             #e.summary = "Some text."
         end
     end
